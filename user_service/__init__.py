1d6fe394c5f773fec23771986ee4108f1e2bcfea
aurel.avramescu <aurel.avramescu@akqa.com>
2014-06-18 02:57:35 +0200

    Refactor imports

--- user_service/__init__.py
+++ user_service/__init__.py
@@ -1,13 +1,13 @@
 # coding: utf-8
 from flask import Flask
-import flask.ext.restless
+from flask.ext.restless import APIManager
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 db = SQLAlchemy()
 
-rest_manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
+rest_manager = APIManager(app, flask_sqlalchemy_db=db)
 from user_service.views import *
 
 
