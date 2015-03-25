9b8c09f46c1a797cdf9f086cdd2a95c1a47e8baa
aav <aav@marketlogicsoftware.com>
2015-03-25 15:56:13 +0100

    Fix python 3.4 support

--- user_service/__init__.py
+++ user_service/__init__.py
@@ -7,11 +7,13 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-rest_manager = APIManager(app, flask_sqlalchemy_db=db)
+rest_manager = APIManager()
 from user_service.views import *
 from user_service.errors import *
 
 
 def init_app(settings='user_service.config'):
     app.config.from_object(settings)
+    db.init_app(app)
+    rest_manager.init_app(app, session=db.session, flask_sqlalchemy_db=db)
 
